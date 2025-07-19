36a5baeac19ce9fb030e53568b547716cc0fcb71
baguwixaezakmi <baguwixaezakmi@gmail.com>
2025-07-19 17:49:27 +0300

    4.1. Аргументы [#295521 #396383]

--- src/main/java/ru/job4j/calculator/ArgumentsMethod.java
+++ src/main/java/ru/job4j/calculator/ArgumentsMethod.java
@@ -8,9 +8,9 @@ public class ArgumentsMethod {
     public static void main(String[] args) {
         String name = "Job4j";
         int age = 6;
-        ArgumentsMethod.hello(name, age);
-        ArgumentsMethod.hello(name, age);
-        ArgumentsMethod.hello(name, age);
-        ArgumentsMethod.hello(name, age);
+        hello(name, age);
+        hello(name, age);
+        hello(name, age);
+        hello(name, age);
     }
 }
