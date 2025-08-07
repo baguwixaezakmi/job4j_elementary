4224f1d2a018dc6e908e13827120e630e16659ea
baguwixaezakmi <baguwixaezakmi@gmail.com>
2025-08-08 02:07:54 +0300

    2. Тесты для задачи "идеальный вес" [#159458 #397403]

---
+++ src/test/java/ru/job4j/calculator/FitTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.calculator;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class FitTest {
+
+    @Test
+    void manWeight() {
+        short input = 180;
+        double expected = 92;
+        double output = Fit.manWeight(input);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void womanWeight() {
+        short input = 170;
+        double expected = 69;
+        double output = Fit.womanWeight(input);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+}
