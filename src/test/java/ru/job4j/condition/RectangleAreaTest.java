3d6b4df6a959842836fff5733a3e68913e1d7ac7
baguwixaezakmi <baguwixaezakmi@gmail.com>
2025-08-10 13:45:53 +0300

    4. Тесты для стороны прямоугольника. [#159460 #397419]

---
+++ src/test/java/ru/job4j/condition/RectangleAreaTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class RectangleAreaTest {
+
+    @Test
+    void whenP6K2Square2() {
+        double p = 6;
+        double k = 2;
+        double expected = 2;
+        double output = RectangleArea.square(p, k);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenP4K1Square1() {
+        double p = 4;
+        double k = 1;
+        double expected = 1;
+        double output = RectangleArea.square(p, k);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+}
