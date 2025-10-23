99273d0e422bf79366f6568a1e88318b4e2d37bf
baguwixaezakmi <baguwixaezakmi@gmail.com>
2025-10-24 02:21:56 +0300

    3. Перегрузить метод max для трех чисел

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -4,4 +4,26 @@ public class Max {
     public static int max(int left, int right) {
         return left > right ? left : right;
     }
+
+//    public static int max(int left, int middle, int right) {
+//        return left > right
+//            ? left > middle
+//                ? left
+//                : middle
+//            : right > middle
+//                ? right
+//                : middle;
+//    }
+
+    public static int max(int left, int middle, int right) {
+        return left > max(middle, right)
+            ? left
+            : max(middle, right);
+    }
+
+    public static int max(int first, int second, int third, int four) {
+        return first > max(second, third, four)
+            ? first
+            : max(second, third, four);
+    }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -40,4 +40,78 @@ class MaxTest {
         int expected = 10;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    void whenLeft4Middle1Right8Max8() {
+        int left = 4;
+        int middle = 1;
+        int right = 8;
+        int result = max(left, middle, right);
+        int expected = 8;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenLeft10Middle5Right8Max10() {
+        int left = 10;
+        int middle = 5;
+        int right = 8;
+        int result = max(left, middle, right);
+        int expected = 10;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenLeft10Middle15Right8Max10() {
+        int left = 10;
+        int middle = 15;
+        int right = 8;
+        int result = max(left, middle, right);
+        int expected = 15;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenFirst10Second15Third8Four4Max15() {
+        int first = 10;
+        int second = 15;
+        int third = 8;
+        int four = 4;
+        int result = max(first, second, third, four);
+        int expected = 15;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenFirst15Second10Third8Four4Max15() {
+        int first = 10;
+        int second = 15;
+        int third = 8;
+        int four = 4;
+        int result = max(first, second, third, four);
+        int expected = 15;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenFirst10Second15Third18Four4Max18() {
+        int first = 10;
+        int second = 15;
+        int third = 18;
+        int four = 4;
+        int result = max(first, second, third, four);
+        int expected = 18;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenFirst10Second15Third18Four40Max40() {
+        int first = 10;
+        int second = 15;
+        int third = 18;
+        int four = 40;
+        int result = max(first, second, third, four);
+        int expected = 40;
+        assertThat(result).isEqualTo(expected);
+    }
 }
